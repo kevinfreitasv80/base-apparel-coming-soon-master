c8c278ab5ade7a4e1247685a58c2f843a9ff0a5a
Kevin Freitas <kevinfreitasv80@gmail.com>
2025-10-03 10:24:12 -0300

    fix javascript

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,20 @@
-/* let form = document.querySelector("#form");
+const form = document.querySelector("#form");
+const errorDescription = document.querySelector(".errorEmail");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  e.currentTarget.
-}); */
+  const email = e.currentTarget.email.value;
+  const regex = RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$");
+
+  if (!regex.test(email)) {
+    if ("error" === form.classList[0]) return;
+
+    form.classList.add("error");
+    errorDescription.hidden = false;
+  }
+  if (regex.test(email) && "error" === form.classList[0]) {
+    form.classList.remove("error");
+    errorDescription.hidden = true;
+  }
+});
